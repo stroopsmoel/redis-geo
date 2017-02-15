ca1f2abedfffc81c00083c229e75edfe9cb8164f
stroopsmoel <stroopsmoel@gmail.com>
2017-02-15 13:04:30 +0100

    Update deploy-envs.sh
    elastic ip address changed

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,7 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=eu-central-1
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=172.31.23.133
+export AWS_VIRTUAL_HOST=35.157.160.107
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
